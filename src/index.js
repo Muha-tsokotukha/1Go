3d2fa243d35443be6d88062ac1b485294189b752
Muha-tsokotukha <vinniedrew7@gmail.com>
2022-12-24 00:51:48 +0600

    fix: path

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ const router = createBrowserRouter([
     element: <MainPage />,
   },
   {
+    path: "/1Go",
+    element: <MainPage />,
+  },
+  {
     path: "/places",
     element: <PlacesPage />,
   },
